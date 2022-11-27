1e643b1ba97289a134803e9c6b4df6515128597a
equilibrium139 <zaidruwaishan@gmail.com>
2022-11-27 11:27:35 -0800

    use glfw and glad for window creation and loading opengl functions

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,6 @@
-#include "tiny_gltf.h"
+#include "tiny_gltf/tiny_gltf.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 using namespace tinygltf;
 
@@ -9,19 +11,57 @@ std::string warn;
 
 int main(int argc, char** argv)
 {
-	bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, argv[1]);
-	//bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, argv[1]); // for binary glTF(.glb)
+	GLFWwindow* window;
 
-	if (!warn.empty()) {
-		printf("Warn: %s\n", warn.c_str());
-	}
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-	if (!err.empty()) {
-		printf("Err: %s\n", err.c_str());
-	}
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-	if (!ret) {
-		printf("Failed to parse glTF\n");
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Failed to initialize GLAD\n");
 		return -1;
-	}
+	}    
+
+    bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, argv[1]);
+    //bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, argv[1]); // for binary glTF(.glb)
+
+    if (!warn.empty()) {
+        printf("Warn: %s\n", warn.c_str());
+    }
+
+    if (!err.empty()) {
+        printf("Err: %s\n", err.c_str());
+    }
+
+    if (!ret) {
+        printf("Failed to parse glTF\n");
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
